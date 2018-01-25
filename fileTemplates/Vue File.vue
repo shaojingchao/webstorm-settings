9ab81afef2000f59e652dd47e79412c0d05181e5
shaojingchao <1247537710@qq.com>
2018-01-25 21:25:21 +0800

    WS-2017.3.1 <12475@DESKTOP-6HFV05I Create baseRefactoring.xml, tpl.tpl, IntelliLang.xml, ide.general.xml, git.xml, _@user_Default.icls, project.default.xml, diff.xml, Monokai copy3.icls, other.xml, Default.xml, file.template.settings.xml, find.xml, ui.lnf.xml, filetypes.xml, Default copy.icls, keymap.xml, code.style.schemes.xml, plugin_ui.xml, Deployment.xml, watcherDefaultTasks.xml, colors.scheme.xml, External Tools.xml, notifications.xml, Vue File.vue, abbreviations.xml, less.less, Default.xml, hg.xml, laf.xml, Seajs JS File.js, Default _2_.xml, path.macros.xml, Monokai copy.icls, Default _1_.xml, web-browsers.xml, proxy.settings.xml, Monokai copy2.icls, editor.rich.copy.xml, editor.xml, usageView.xml, webServers.xml, debugger.xml

---
+++ fileTemplates/Vue File.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="">
+    
+  </div>
+</template>
+
+<style lang="less" rel="stylesheet/less" scoped>
+</style>
+
+<script type="text/ecmascript-6">
+  import HeaderComponent from './components/header.vue'
+  export default {
+    data () {
+      return {
+        msg: 'hello vue'
+      }
+    },
+    components: {
+      HeaderComponent
+    }
+  }
+</script>
